778b4d54e1fb40c435d44d3d6852a13c8ddff7ad
Maria Skrzypek <mmskrzyp@gmail.com>
2012-02-05 21:34:30 +0100

    stars shining bright above you, night breezes seems to whisper "I love you"...

--- src/pl/edu/pk/buckling/CalculateFormActivity.java
+++ src/pl/edu/pk/buckling/CalculateFormActivity.java
@@ -1,5 +1,8 @@
 package pl.edu.pk.buckling;
 
+import java.math.BigDecimal;
+
+import pl.edu.pk.beam.Beam;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,16 +15,26 @@ public class CalculateFormActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
-	setContentView(R.layout.calculate_beam_layout);
-
-	// TODO
+	setContentView(R.layout.calculate_beam_layout);	
+	
 	Button btCalculate = (Button) findViewById(R.id.btCalculate);
 	btCalculate.setOnClickListener(new OnClickListener() {
 
 	    public void onClick(View v) {
-		startActivity(new Intent(getBaseContext(),
-			CalculationResultActivity.class));
+		Intent intent = new Intent(getBaseContext(),
+			CalculationResultActivity.class);
+		intent.putExtra(RESULT, calulateFromForm());
+		startActivity(intent);
 	    }
 	});
     }
+    
+    private BigDecimal calulateFromForm(){
+	Beam beam = new Beam(); //na podstawie formularza
+	// Beam beam = new Beam(mass, length, ...);
+	BigDecimal result = beam.getCriticalPower();
+	return result;
+    }
+    
+    public static final String RESULT = "result";
 }
--- src/pl/edu/pk/buckling/CalculationResultActivity.java
+++ src/pl/edu/pk/buckling/CalculationResultActivity.java
@@ -1,7 +1,10 @@
 package pl.edu.pk.buckling;
 
+import java.math.BigDecimal;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CalculationResultActivity extends Activity {
 
@@ -9,8 +12,11 @@ public class CalculationResultActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.calculation_result_layout);
-
-	// TODO
+	
+	Bundle bundle = getIntent().getExtras();
+	BigDecimal result = (BigDecimal) bundle.get(CalculateFormActivity.RESULT);
+	TextView tvResult = (TextView) findViewById(R.id.tvResult);
+	tvResult.setText(result.toString());
     }
 
 }
